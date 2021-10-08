793d63d31165d5869917f6ad987e81935631ec18
Carl Avidano <cavidano@gmail.com>
2021-10-08 08:41:00 -0400

    adding files

---
+++ .fantasticonrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  inputDir: './original-svg', // (required)
+  outputDir: './dist', // (required)
+  fontTypes: ['ttf', 'woff2', 'woff'],
+  assetTypes: ['css', 'json', 'html'],
+  name: 'natura11y-icons',
+  prefix: 'icon-',
+  selector: null,
+  fontsUrl: './fonts',
+  formatOptions: {
+    json: {
+      indent: 3
+    }
+  },
+  pathOptions: {
+    json: './dist/natura11y-icons.json',
+    css: './dist/natura11y-icons.css',
+    html: './dist/index.html',
+    ttf: './dist/fonts/natura11y-icons.ttf',
+    woff: './dist/fonts/natura11y-icons.woff',
+    woff2: './dist/fonts/natura11y-icons.woff2'
+  }
+};
