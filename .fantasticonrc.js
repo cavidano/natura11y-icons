93a3a2e2fb811f35e787321b5e82a75e94b10c45
Carl Avidano <cavidano@gmail.com>
2023-10-23 09:10:48 -0400

    cleaning up settings

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,13 +1,13 @@
-const codepoints = require('./dist/natura11y-icons.json');
+const codepoints = require('./natura11y-icons.json');
 
 module.exports = {
-  inputDir: './original-svg', // (required)
+  inputDir: './optimized-svg', // (required)
   outputDir: './dist', // (required)
   fontTypes: ['ttf', 'woff2', 'woff'],
   assetTypes: ['css', 'json', 'html'],
   name: 'natura11y-icons',
   codepoints: codepoints,
-  prefix: 'icon',
+  // prefix: null,
   selector: '.icon',
   tag: 'span',
   formatOptions: {
@@ -16,9 +16,9 @@ module.exports = {
     }
   },
   pathOptions: {
+    json: './natura11y-icons.json',
     html: './dist/index.html',
     css: './dist/natura11y-icons.css',
-    json: './dist/natura11y-icons.json',
     ttf: './dist/natura11y-icons.ttf',
     woff: './dist/natura11y-icons.woff',
     woff2: './dist/natura11y-icons.woff2'
