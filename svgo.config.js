1675623fbd4c9121896601d225f9734304a7606c
Carl Avidano <cavidano@gmail.com>
2023-10-27 07:06:37 -0400

    reverting legacy icons

--- svgo.config.js
+++ svgo.config.js
@@ -1,27 +1,29 @@
-'use strict';
+'use strict'
 
 module.exports = {
-	multipass: true,
-	js2svg: {
-		pretty: true,
-		indent: 3,
-	},
-	plugins: [
-		{
-			name: 'preset-default',
-			params: {
-				overrides: {
-					cleanupListOfValues: true,
-					removeAttrs: {
-						attrs: ['data-name', 'fill', 'clip-rule'],
-					},
-					removeUnknownsAndDefaults: {
-						keepRoleAttr: true,
-					},
-					removeViewBox: false,
-					sortAttrs: true,
-				},
-			},
-		},
-	],
-};
+  multipass: true,
+  js2svg: {
+    pretty: true,
+    indent: 2
+  },
+  plugins: [{
+    name: 'preset-default',
+    params: {
+      overrides: {
+        cleanupListOfValues: true,
+        removeAttrs: {
+          attrs: [
+            'data-name',
+            'fill',
+            'clip-rule'
+          ]
+        },
+        removeUnknownsAndDefaults: {
+          keepRoleAttr: true
+        },
+        removeViewBox: false,
+        sortAttrs: true
+      }
+    }
+  }]
+}
